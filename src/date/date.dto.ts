b865e19dd669407a2cc9d45a8c30c08e2f46f8bc
xionkq <3030199850@qq.com>
2023-05-22 20:04:41 +0800

    update date Schema

--- src/date/date.dto.ts
+++ src/date/date.dto.ts
@@ -1,7 +1,7 @@
 // 新建用户
 export class CreateDateDTO {
   readonly user_name: string;
-  readonly project_id: number;
+  readonly project_name: string;
   readonly date: number;
   readonly duration: number;
   readonly message: string;
