9cd8b35f1be52bcbcb3aff826f568c31dddcc937
xionkq <3030199850@qq.com>
2023-05-24 19:50:37 +0800

    update

--- src/date/date.interface.ts
+++ src/date/date.interface.ts
@@ -2,7 +2,7 @@ import { Document } from 'mongoose';
  
 export interface Date extends Document {
   readonly user_name: string;
-  readonly project_id: number;
+  readonly project_name: string;
   readonly date: number;
   readonly duration: number;
   readonly message: string;
